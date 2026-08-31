448a94fe0cec708f52e85c6265e299b918531072
Juliana Vieira <julianavs.leal@gmail.com>
2026-08-31 14:52:12 -0300

    Initial commit

---
+++ Programa.cs
@@ -0,0 +1,17 @@
+﻿class Programa
+{
+    static void Main()
+    {
+        Musica musica1 = new Musica() { Nome = "Roxane", Artista = "The Police" };
+        musica1.Duracao = 273;
+        musica1.Disponivel = true;
+        Console.WriteLine(musica1.DescricaoResumida);
+
+        Musica musica2 = new Musica() { Nome = "Vertigo", Artista = "U2" };
+        musica2.Duracao = 367;
+        musica2.Disponivel = false;
+
+        musica1.ExibirFichaTecnica();
+        musica2.ExibirFichaTecnica();
+    }
+}
