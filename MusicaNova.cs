4b138717b5d25e61b7871ff5ab199c16956a70c4
Juliana Vieira <julianavs.leal@gmail.com>
2026-09-03 14:56:52 -0300

    Adicionar as classes

---
+++ MusicaNova.cs
@@ -0,0 +1,8 @@
+
+class MusicaNova{
+  public string nome;
+  public DateTime dataLancamento;
+  public int duracao;
+  public bool disponivel;
+}
+
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿﻿class Program
+{
+	public static void CriarMusica()
+	{
+		MusicaNova musica1 = new MusicaNova();
+		musica1.nome = "Roxane";
+		musica1.dataLancamento = DateTime.Parse("12/12/2008");
+	}
+}
